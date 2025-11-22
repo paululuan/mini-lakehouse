98fb3e50406391dc43e294c2c6943f277ca6890b
paululuan <paululuan@gmail.com>
2025-11-22 11:15:04 -0300

    adicao do adminer para postgres no docker compose e script de criacao de schemas

---
+++ sql/create_schemas.sql
@@ -0,0 +1,15 @@
+-- Criar schemas principais
+CREATE SCHEMA IF NOT EXISTS "raw-layer";
+CREATE SCHEMA IF NOT EXISTS "bronze-layer";
+CREATE SCHEMA IF NOT EXISTS "silver-layer";
+CREATE SCHEMA IF NOT EXISTS "gold-layer";
+
+-- Criar sandbox para usuário
+CREATE SCHEMA IF NOT EXISTS "sandbox-luan";
+
+-- Opcional: dar permissões ao usuário airflow
+GRANT USAGE ON SCHEMA "raw-layer" TO airflow;
+GRANT USAGE ON SCHEMA "bronze-layer" TO airflow;
+GRANT USAGE ON SCHEMA "silver-layer" TO airflow;
+GRANT USAGE ON SCHEMA "gold-layer" TO airflow;
+GRANT USAGE, CREATE ON SCHEMA "sandbox-luan" TO airflow;
